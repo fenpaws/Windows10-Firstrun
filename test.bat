e9f088641f40bf4e4a2752b7ce47b8fc37167e23
SoXX <SoXX-TheFurry@outlook.de>
2018-04-10 14:14:19 +0200

    initial commit
    First version of my Tool

---
+++ test.bat
@@ -0,0 +1,38 @@
+@echo off & setlocal
+
+:: BatchGotAdmin
+::-------------------------------------
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"="
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+::--------------------------------------
+
+
+::-----Chocolatey Install Scrypt
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
+::-----Start Intall loop
+for /f "delims=," %%a in (.\install.txt) do choco install %%a --acceptlicense -y
+
+::-----Reclaim Windows
+git clone https://github.com/Disassembler0/Win10-Initial-Setup-Script.git
+::powershell.exe -NoProfile -ExecutionPolicy Bypass -File .\Win10-Initial-Setup-Script\Win10.ps1 -preset .\reclaimPreset.txt
+pause
