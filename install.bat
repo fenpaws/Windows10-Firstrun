bb29326ea34899bdb5e4e1e379454ed155b714b8
SoXX <SoXX-TheFurry@outlook.de>
2018-04-10 14:16:41 +0200

    renamed choco install file

--- install.bat
+++ install.bat
@@ -30,7 +30,7 @@ if '%errorlevel%' NEQ '0' (
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
 ::-----Start Intall loop
-for /f "delims=," %%a in (.\install.txt) do choco install %%a --acceptlicense -y
+for /f "delims=," %%a in (.\choco.txt) do choco install %%a --acceptlicense -y
 
 ::-----Reclaim Windows
 git clone https://github.com/Disassembler0/Win10-Initial-Setup-Script.git
